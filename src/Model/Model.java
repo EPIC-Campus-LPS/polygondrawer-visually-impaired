3497ad584a0dce3bc381d02d484f106ca493b1d6
connorb809_lpsk12 <Connorb809@lpsk12.org>
2024-02-09 09:35:14 -0700

    Model class Java Doc Comments for Model

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,11 +1,53 @@
+
+import java.awt.Polygon;
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.Optional;
+
 /**
- *
  * @author Nathan Hoehndorf and Connor Bonn
  * @since February 9, 2024
- *
  */
 public class Model {
+    //used to keep track of polygons
+    LinkedList<Polygon> list = new LinkedList<>();
+    //used to keep track of current polygon
+    Polygon current = null;
 
+    /**
+     * Returns polygons in the model
+     *
+     * @return
+     */
+    public Iterator<Polygon> iterator() {
+        return list.iterator();
+    }
 
+    /**
+     * Returns the total number of polygons
+     * @return
+     */
+    public int size() {
+        //returns the number of polygons
+        return list.size();
+    }
 
+    /**
+     * Removes all polygons from the list
+     */
+    public void removeAll() {
+        //removes all the polygons from the list
+        list.clear();
+        //removes current
+        current = null;
+    }
+
+    /**
+     * Adds a polygon to the list
+     * @param poly
+     */
+    public void addPolygon(Polygon poly) {
+        //adds a Polygon
+        list.add(poly);
+    }
 }
