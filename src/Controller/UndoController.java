7b489eee0a0812f52099ea1402dbab60a29a2516
nxthyn <nathanh236@lpsk12.org>
2024-02-14 09:22:11 -0700

    Completed and generated JavaDoc

--- src/Controller/UndoController.java
+++ src/Controller/UndoController.java
@@ -10,32 +10,47 @@ import java.util.Optional;
  *
  */
 public class UndoController {
+    /**
+     * Instance of model class
+     */
     private Model model;
+    /**
+     * Instance of application class
+     */
     private Application app;
 
+    /**
+     * Default constructor
+     * @param app
+     * @param m
+     *
+     */
     public UndoController(Application app, Model m) {
         this.app = app;
         this.model = m;
     }
 
+    /**
+     * Removes most recent point in polygon
+     */
     public void removeLastPoint() {
-        Optional<Polygon> selected = model.getSelected();
-        if (selected.isPresent()) {
-            Polygon poly = selected.get();
-            if (poly.npoints > 0) {
-                poly.npoints--;
+        Optional<Polygon> selected = model.getSelected(); // gets the selected (and therefore most recent) point
+        if (selected.isPresent()) { // if a point is selected
+            Polygon poly = selected.get(); // save the point
+            if (poly.npoints > 0) { // if there is at least one point
+                poly.npoints--; // decrement the number of points
             }
-        } else {
-            Iterator<Polygon> it = model.iterator();
-            while (it.hasNext()) {
+        } else { // a point is not selected
+            Iterator<Polygon> it = model.iterator(); // holds all the points
+            while (it.hasNext()) { // not at end
                 it.next();
                 if (!it.hasNext()) {
-                    it.remove();
+                    it.remove(); // remove the last one
                     break;
                 }
             }
         }
-        UpdateMenu.updateMenu(app, model);
-        app.repaint();
+        UpdateMenu.updateMenu(app, model); // updates the menu bar
+        app.repaint(); // refreshes the screen
     }
 }
